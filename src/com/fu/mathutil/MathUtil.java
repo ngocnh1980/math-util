fa94f05851f084de9d75a52dd856b39149c619ac
ngocnh1980 <nhong.ngoc1980@gmail.com>
2023-02-22 15:46:02 +0700

    lan dau up code len server

---
+++ src/com/fu/mathutil/MathUtil.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.fu.mathutil;
+
+/**
+ *
+ * @author rumina
+ */
+public class MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        long result = MathUtilLive.getFactorial(5);
+        System.out.println("expected: 5! = 120; actual:" + result);
+    }
+    
+}
